2efc57e4fb7b74417b7846f0bd05151e0efc2cad
Mihai <mihaiandrei.sandu@ulbsibiu.ro>
2026-03-17 11:11:06 +0200

    Laborator 3 alpha

---
+++ src/Laborator3/Main_Lab3.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+void main() throws IOException {
+
+    String input = Files.readString(Path.of("src/Laborator3/in.txt"));
+
+    System.out.println("Input: \n" + input + "\n");
+
+    System.out.println("Separat: \n");
+    String[] props = input.split("\\.");
+    for(String w: props) {
+        System.out.print("[" + w.trim() + "] \n");
+    }
+
+}
